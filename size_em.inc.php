e1009b944ba51d360d33f99811c7b1c29729a6a3
toooooooby <toby.net.info.mail+git@gmail.com>
2009-10-04 13:01:18 +0900

    implemented multi block(_convert)

--- size_em.inc.php
+++ size_em.inc.php
@@ -1,30 +1,47 @@
 <?php
-// PukiWiki - Yet another WikiWikiWeb clone.
-// $Id: size.inc.php,v 1.10 2005/06/16 15:04:08 henoheno Exp $
-//
-// Text-size changing via CSS plugin
+// http://github.com/toooooooby/pukiwiki-size_em-plugin/
 
-define('PLUGIN_SIZE_EM_MAX', 5); // px
-define('PLUGIN_SIZE_EM_MIN',  0.1); // px
+define('PLUGIN_SIZE_EM_MAX', 5); // em
+define('PLUGIN_SIZE_EM_MIN',  0.1); // em
 
 // ----
-define('PLUGIN_SIZE_EM_USAGE', '&size_em(em){Text you want to change};');
+define('PLUGIN_SIZE_EM_USAGE', 'size_em.inc.php usage: &size_em(em){Text you want to change};');
+define('PLUGIN_SIZE_EM_CONVERT_USAGE', "size_em.inc.php usage: #size_em(em){{</br>\nText you want to change</br>\n}}</br>\n");
+
+function _plugin_size_em_get_args($func_get_args)
+{
+  $func_num_args = count($func_get_args);
+  if ($func_num_args != 2) return array(FALSE, "", "");
+  
+  list($size, $body) = $func_get_args;
+  if ($size == '' || $body == '' || ! preg_match('/^\d+$/', $size))
+      return array(FALSE, "", "");
+  $size = max(PLUGIN_SIZE_EM_MIN, min(PLUGIN_SIZE_EM_MAX, $size));
+  return array(TRUE, $size, $body);
+}
+
 
 function plugin_size_em_inline()
 {
-	if (func_num_args() != 2) return PLUGIN_SIZE_EM_USAGE;
+  $func_get_args = func_get_args();
+  list($result, $size, $body) = _plugin_size_em_get_args($func_get_args);
+  if ($result) {
+    return '<span style="font-size:' . $size .
+      'em;display:inline-block;line-height:100%;text-indent:0px">' .
+      $body . '</span>';
+  } else {
+    return PLUGIN_SIZE_EM_USAGE;
+  }
+}
 
-	list($size, $body) = func_get_args();
-
-	// strip_autolink() is not needed for size plugin
-	//$body = strip_htmltag($body);
-	
-	if ($size == '' || $body == '' || ! preg_match('/^\d+$/', $size))
-		return PLUGIN_SIZE_EM_USAGE;
-
-	$size = max(PLUGIN_SIZE_EM_MIN, min(PLUGIN_SIZE_EM_MAX, $size));
-	return '<span style="font-size:' . $size .
-		'em;display:inline-block;line-height:100%;text-indent:0px">' .
-		$body . '</span>';
+function plugin_size_em_convert()
+{
+  $func_get_args = func_get_args();
+  list($result, $size, $body) = _plugin_size_em_get_args($func_get_args);
+  if ($result) {
+    return '<div style="font-size:' . $size . '">' . $body . '</div>';
+  } else {
+    return PLUGIN_SIZE_EM_CONVERT_USAGE;
+  }
 }
 ?>
